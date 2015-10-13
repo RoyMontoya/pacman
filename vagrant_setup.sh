058099c5cf42d17db14e5004fdab4e851b1b6948
Isaac Zepeda <keogh24@gmail.com>
2015-10-12 21:01:25 -0700

    Vagrant setup improvements

--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 
+echo "Updating Ubuntu"
 sudo apt-get -y update
-sudo apt-get -y install git-core nodejs 
+
+echo "Installing node and git-core"
+sudo apt-get -y install git-core nodejs
 
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 
+echo "Installing npm"
 sudo apt-get -y install npm
 
-sudo npm install -g jspm
+echo "Installing gulp and jspm"
 sudo npm install -g gulp
+sudo npm install -g jspm
+sudo npm install -g jspm-bower-endpoint
 
+echo "Registering jspm-bower-endpoint"
+jspm registry create bower jspm-bower-endpoint
+
+sudo chown -R $(whoami) ~/.npm
+sudo chown -R $(whoami) ~/tmp
+sudo chown -R $(whoami) /usr/local/lib/node_modules
+
+echo "Installing dependencies"
 cd /vagrant
-npm install
 jspm install
+sudo npm install
+
+npm link gulp
