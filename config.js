710cfc471814c3bef9f94034af945dbcd39704e9
Isaac Zepeda <keogh24@gmail.com>
2015-10-12 21:01:11 -0700

    Installing lodash

--- config.js
+++ config.js
@@ -17,6 +17,7 @@ System.config({
     "babel": "npm:babel-core@5.8.25",
     "babel-runtime": "npm:babel-runtime@5.8.25",
     "core-js": "npm:core-js@1.2.1",
+    "lodash": "npm:lodash@3.10.1",
     "phaser": "github:photonstorm/phaser@2.4.3",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
@@ -28,6 +29,9 @@ System.config({
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:lodash@3.10.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
 });
