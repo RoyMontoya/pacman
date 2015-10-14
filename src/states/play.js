c706b0451a9d0b636a23550974534b6319b08d3f
Isaac Zepeda <keogh24@gmail.com>
2015-10-14 15:49:36 -0700

    Adding play skeleton

---
+++ src/states/play.js
@@ -0,0 +1,29 @@
+
+export default Pacman;
+
+function Pacman(game) {
+
+}
+
+Pacman.prototype = {
+  init: function () {
+
+  },
+
+  preload: function () {
+
+  },
+
+  create: function () {
+
+  },
+
+  update: function () {
+
+  },
+
+  render: function () {
+
+  }
+
+};
